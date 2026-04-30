5630275d3894e6363181611570e9ef423663256c
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2026-05-01 04:08:56 +0600

    primitive data type check

---
+++ src/primitive.ts
@@ -0,0 +1,15 @@
+
+ // primitive javascript- number,boolean,string,undefined,null
+  //           typescript- never,unknown,void with javascript
+
+  let userName:string="gaurav sen gupto"
+  userName='dip' // resign as string but cannot assign with number like javascript1
+  console.log(userName);
+
+  let isError=false;
+  isError=true
+
+  let roll:undefined=undefined
+ 
+  // in here roll data type is any you can assign any kind of value but when you set its type you will not change
+  console.log(roll);
