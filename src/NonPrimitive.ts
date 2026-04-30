ad2c586eaf8bea1bd680567061a53dd6d834d77b
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2026-05-01 04:37:00 +0600

    non primitive data type array and tuple

---
+++ src/NonPrimitive.ts
@@ -0,0 +1,11 @@
+//Non primitive - Array,Object,Tuple ---Mutable
+
+const fruitList:(string | number)[]=['apple','custard_apple','orange',13]
+
+fruitList.push(12)
+console.log(fruitList);
+
+//tuple
+
+const result:[string,number,number]=['Physics',84,5]
+console.log(result);
